092293090ef5a28ecd5d33bf937a1eb95459e718
Kim Fitter <kimfitter@yahoo.com>
2021-07-16 17:49:03 +1200

    Add boilerplate presentation

---
+++ main.js
@@ -0,0 +1,6 @@
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/night.css'
+import Reveal from 'reveal.js'
+
+const deck = new Reveal()
+deck.initialize({ hash: true, slideNumber: true })
